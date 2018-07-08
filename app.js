c53da92d51abb659e6212318f2e0563286d669cc
Marcus Veloso <desenvolvedoroficial@gmail.com>
2018-07-08 11:06:08 -0300

    Configurar socket.io

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var app = require('./config/server');
 
-app.listen(3002, function(){
+var server = app.listen(3002, function () {
     console.log("Server ONLINE");
+});
+
+var io = require('socket.io').listen(server);
+
+//CONEXÃO POR WEBSOCKET
+io.on('connection', function (socket) {
+    console.log('Conectou');
+    
+    socket.on('disconnect', function () {
+        console.log('Usuario desconectou');
+    });
 });
