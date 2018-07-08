e4121e31bb9fea83a016390d82126c0ea9f787ee
Marcus Veloso <desenvolvedoroficial@gmail.com>
2018-07-08 10:42:21 -0300

    Criar login

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,17 @@
 module.exports.iniciaChat = function (application, req, res) {
+
+    var dadosForm = req.body;
+
+    req.assert('apelido', 'Apelido deve ser informado!').notEmpty();
+    req.assert('apelido', 'Apelido deve conter entre 3 e 15 caracteres!').len(5, 15);
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        console.log(erros);
+        res.render('index', { validacao: erros });
+        return;
+    }
+
     res.render('chat');
 }
